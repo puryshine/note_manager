3e665cb8c0dbb449cb582f679d032e74c6d4ed02
puryshine <raleonov98@gmail.com>
2024-12-17 22:19:53 +0900

    qq

---
+++ add_list.py
@@ -0,0 +1,4 @@
+first_add, second_add, third_add = input('Введите первый заголовок'), \
+    input('Введите второй заголовок'), \
+    input('Введите третий заголовок')
+print(first_add, second_add, third_add)
---
+++ date_changer.py
@@ -0,0 +1,5 @@
+def date_changer(_):
+    return(print(*(_.day, _.month)))            # предоставление даты в формате: "день месяц"
+
+# created_date = date.today()
+# date_changer(created_date)
