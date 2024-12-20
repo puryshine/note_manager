cb90d5e03527863cf8164ebfe9bc621f9f03fa8c
puryshine <Raleonov98@gmail.com>
2024-12-20 11:04:21 +0900

    qq

--- add_titles_loop.py
+++ add_titles_loop.py
@@ -1,6 +1,6 @@
 from add_list import heads
-zapros = input("Введите заголовок, для отмены оставьте строчку пустой")
+zapros = input("добавьте заголовок, для отмены оставьте строчку пустой")
 while zapros != '':
     heads.append(zapros)
-    zapros = input("Введите заголовок, для отмены оставьте строчку пустой")
-    print(*heads)
+    zapros = input("добавьте заголовок, для отмены оставьте строчку пустой")
+    print(f'{heads}')
