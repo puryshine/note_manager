8cf1956b6e1eadb8a28898fc9080e0368ad8c31a
puryshine <Raleonov98@gmail.com>
2024-12-21 14:18:00 +0900

    визуал

--- add_titles_loop.py
+++ add_titles_loop.py
@@ -6,7 +6,9 @@ while zapros != '':      # цикл для реализации повторно
         heads.append(zapros) # добавляем новый заголовок в конец списка, если таковой имеется
     else:
         print('Такой заголовок уже имеется')
-    print(*"Заголовки: \n", heads)
+    print("Заголовки:")
+    print(*heads)
     zapros = input("Добавьте заголовок, для отмены оставьте строчку пустой")
 heads = tuple(heads)
-print(*"Заголовки: \n", heads)
+print("Заголовки:")
+print(*heads)
