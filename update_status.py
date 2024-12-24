6444312d66f4f14b031f70068a9023408c04031e
puryshine <Raleonov98@gmail.com>
2024-12-24 18:32:42 +0900

    удалил старую

--- update_status.py
+++ update_status.py
@@ -1,25 +1,6 @@
 # функциональность статуса на основе данных
 import status_
 
-
-
-# def update_status(
-#         request):  # :) нет слов и идей. Всё что я пробовал, в finals не работает, тк импорт не перезаписывается
-#     if request == 'да':
-#         status_temp = input("Введите статус заметки буквой: А — Активный, О — Отменен, З — Завершен").lower()
-#         status_count = False
-#         while status_count == False:
-#             if status_temp != "а" and status_temp != "о" and status_temp != "з":
-#                 status_temp = input(
-#                     "Введите статус заметки буквой: А — Активный, О — Отменен, З — Завершен"
-#                     ).lower()
-#             if status_temp == "а" and status_temp == "о" and status_temp == "з":
-#                 status_count = True
-#     else:
-#         break
-#     return status_temp
-
-
 def update_status(
         request):
     if request == 'да':
