acd4f23d54fdf6635efa7a3e083047cf1dd27e66
puryshine <Raleonov98@gmail.com>
2025-01-03 22:39:41 +0900

    оптимизировал

--- status_.py
+++ status_.py
@@ -1,14 +1,8 @@
 status_lib = {"а": 'Активный', "о": 'Отменен', "з": 'Завершен'}
 status_temp = 0
 status_count = False
-while status_count == False:
-    if status_temp != "а" and status_temp != "о" and status_temp != "з":
-        status_temp = input("Введите статус заметки буквой: А — Активный, О — Отменен, З — Завершен").lower()
-        status_count = False
-    else:
-        status_count = True
-
-status = (
-status_lib[status_temp])  # защита, чтобы один из трех вариантов всё-же выбрали и программа не улетала в ошибку
+while status_temp not in ["а", "о", "з"]:
+    status_temp = input("Введите статус заметки буквой: А — Активный, О — Отменен, З — Завершен").lower()
+status = status_lib[status_temp]
 
 print(status)
