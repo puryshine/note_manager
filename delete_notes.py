581bd275d57d40652d2aac2bf5de4e575575ca79
puryshine <Raleonov98@gmail.com>
2025-01-06 23:24:06 +0900

    не тестил убрал обрамление в функцию, делал потому что не верно интепритировал задание :)

--- delete_notes.py
+++ delete_notes.py
@@ -1,28 +1,29 @@
-def delete_notes(notes):
-    while True:
-        add_input = input("Желаете удалить заметку? Да/Нет").lower()
-        # используется add_input, т.к. add_input.py использвать не планирую
-        if add_input == 'да':
-            break
-        elif add_input == 'нет':
-            break
-    if add_input == "нет":
+# Удаляет заметки
+
+while True:
+    add_input = input("Желаете удалить заметку? Да/Нет").lower()
+    # используется add_input, т.к. add_input.py использвать не планирую
+    if add_input == 'да':
         break
-    find_key = input("Введите имя пользователя или заголовок заметки \nкоторый желаете удалить").lower().strip()
-    for _ in range(len(notes)):
-        if find_key in notes[_].get('Имя') and notes[_].get("Заголовки"):
-            print(f"Такое имя пользователя/заголовок ({find_key}) найдено!")
-            print(f'Вот заметка: {notes[_].get("Имя"), notes[_].get("Заголовки")}') # В терминал
-            while True:
-                add_input = input("Удалить?(да/нет)").lower()
-                if add_input == "да":
-                    notes.pop(_)
-                    break
-                elif add_input == "нет":
-                    break
-        else:
-            print(f"{find_key} не найден")
-    for _ in range(len(notes)):  # вызов названий заметок путем перебора всех имеющихся
-        print("Текущий список заметок:")
-        print(f'Заметка №{_ + 1}:')
-        print(notes[_].get("Имя"), notes[_].get("Заголовки"))
+    elif add_input == 'нет':
+        break
+if add_input == "нет":
+    break
+find_key = input("Введите имя пользователя или заголовок заметки \nкоторый желаете удалить").lower().strip()
+for _ in range(len(notes)):
+    if find_key in notes[_].get('Имя') and notes[_].get("Заголовки"):
+        print(f"Такое имя пользователя/заголовок ({find_key}) найдено!")
+        print(f'Вот заметка: {notes[_].get("Имя"), notes[_].get("Заголовки")}')  # В терминал
+        while True:
+            add_input = input("Удалить?(да/нет)").lower()
+            if add_input == "да":
+                notes.pop(_)
+                break
+            elif add_input == "нет":
+                break
+    else:
+        print(f"{find_key} не найден")
+for _ in range(len(notes)):  # вызов названий заметок путем перебора всех имеющихся
+    print("Текущий список заметок:")
+    print(f'Заметка №{_ + 1}:')
+    print(notes[_].get("Имя"), notes[_].get("Заголовки"))
--- multiple_notes.py
+++ multiple_notes.py
@@ -1,51 +1,50 @@
 from datetime import datetime
 
-# status_lib = {"а": 'Активный', "о": 'Отменен', "з": 'Завершен'}
-# notes = []
+# Добавляет заметки
+notes = []
 
-def multiple_notes(notes):
+while True:
+    status_lib = {"а": 'Активный', "о": 'Отменен', "з": 'Завершен'}
+    name = input("Введите имя пользователя")
+    heads = input('Введите заголовок')
+    content = input("Введите содержание заметки")
+    status_temp = 0
+    while status_temp not in ["а", "о", "з"]:
+        status_temp = input("Введите статус заметки буквой: А — Активный, О — Отменен, З — Завершен").lower()
+    status = status_lib[status_temp]
     while True:
-        status_lib = {"а": 'Активный', "о": 'Отменен', "з": 'Завершен'}
-        name = input("Введите имя пользователя")
-        heads = input('Введите заголовок')
-        content = input("Введите содержание заметки")
-        status_temp = 0
-        while status_temp not in ["а", "о", "з"]:
-            status_temp = input("Введите статус заметки буквой: А — Активный, О — Отменен, З — Завершен").lower()
-        status = status_lib[status_temp]
-        while True:
-            try:
-                created_date = datetime.strptime(input("Введите дату создания в формате ДД-ММ-ГГГГ"), '%d-%m-%Y')
-                break
-            except ValueError:
-                print("Неверный формат даты, попробуйте ещё раз")
-
-        while True:
-            try:
-                issue_date = datetime.strptime(input("Введите деадлайн в формате ДД-ММ-ГГГГ"), '%d-%m-%Y')
-                break
-            except ValueError:
-                print("Неверный формат даты, попробуйте ещё раз")
-
-        note = {"Имя": name,
-                "Заголовки": heads,
-                "Содержание": content,
-                "Статус": status,
-                "Дата создания": created_date,
-                "Дата окончания": issue_date
-                }
-        notes.append(note)
-
-        for _ in range(len(notes)): # вызов названий заметок путем перебора всех имеющихся
-            print(f'Заметка №{_ + 1}:')
-            print(notes[_].get("Имя"), notes[_].get("Заголовки"))
-
-        while True:
-            add_input = input("Добавить ещё заметок? да/нет").lower()
-            # используется add_input, т.к. add_input.py использвать не планирую
-            if add_input == 'да':
-                break
-            elif add_input == 'нет':
-                break
-        if add_input == 'нет':
+        try:
+            created_date = datetime.strptime(input("Введите дату создания в формате ДД-ММ-ГГГГ"), '%d-%m-%Y')
             break
+        except ValueError:
+            print("Неверный формат даты, попробуйте ещё раз")
+
+    while True:
+        try:
+            issue_date = datetime.strptime(input("Введите деадлайн в формате ДД-ММ-ГГГГ"), '%d-%m-%Y')
+            break
+        except ValueError:
+            print("Неверный формат даты, попробуйте ещё раз")
+
+    note = {"Имя": name,
+            "Заголовки": heads,
+            "Содержание": content,
+            "Статус": status,
+            "Дата создания": created_date,
+            "Дата окончания": issue_date
+            }
+    notes.append(note)
+
+    for _ in range(len(notes)):  # вызов названий заметок путем перебора всех имеющихся
+        print(f'Заметка №{_ + 1}:')
+        print(notes[_].get("Имя"), notes[_].get("Заголовки"))
+
+    while True:
+        add_input = input("Добавить ещё заметок? да/нет").lower()
+        # используется add_input, т.к. add_input.py использвать не планирую
+        if add_input == 'да':
+            break
+        elif add_input == 'нет':
+            break
+    if add_input == 'нет':
+        break
