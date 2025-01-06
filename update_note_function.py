f56d232ba98922d8c878b1c76d15d1267fd6f74c
puryshine <Raleonov98@gmail.com>
2025-01-07 02:37:37 +0900

    без теста
    подтверждение обновления

--- update_note_function.py
+++ update_note_function.py
@@ -1,31 +1,65 @@
+from datetime import datetime
+
+
 def update_note_function(note):  # в задании нет перебора по заметкам, думаю функцию можно применять по индексу
     print(f'Текущие данные заметки:\n{note}')
     while True:
         print("Поля заметки:\nИмя, Заголовки, Содержание, Статус, Дата окончания")
         add_input = input("Введите поле заметки, что хотите изменить").capitalize()
         if add_input == "Заголовки":
-            heads = input("Введите новый заголовок")
+            while True:
+                add_input = input("Желаете заголовки?(да/нет)").lower()
+                if add_input == "да":
+                    heads = input("Введите новый заголовок")
+                    break
+                elif add_input == 'нет':
+                    break
         elif add_input == "Содержание":
-            content = input("Введите новое содержание")
-        elif add_input == "Имя"
-            name = input("Введите новое имя пользователя")
+            while True:
+                add_input = input("Желаете содержание заметки?(да/нет)").lower()
+                if add_input == "да":
+                    content = input("Введите новое содержание")
+                    break
+                elif add_input == 'нет':
+                    break
+        elif add_input == "Имя":
+            while True:
+                add_input = input("Желаете имя пользователя?(да/нет)").lower()
+                if add_input == "да":
+                    name = input("Введите новое имя пользователя")
+                    break
+                elif add_input == 'нет':
+                    break
         elif add_input == "Дата окончания":
             while True:
-                try:
-                    issue_date = datetime.strptime(input("Введите дедлайн в формате ДД-ММ-ГГГГ"), '%d-%m-%Y')
+                add_input = input("Желаете дедлайн?(да/нет)").lower()
+                if add_input == "да":
+                    while True:
+                        try:
+                            issue_date = datetime.strptime(input("Введите дедлайн в формате ДД-ММ-ГГГГ"), '%d-%m-%Y')
+                            break
+                        except ValueError:
+                            print("Неверный формат даты, попробуйте ещё раз")
+                            break
+                elif add_input == 'нет':
                     break
-                except ValueError:
-                    print("Неверный формат даты, попробуйте ещё раз")
         elif add_input == "Статус":
-            status_temp = input("Введите статус заметки буквой: А — Активный, О — Отменен, З — Завершен").lower()
             while True:
-                if status_temp != "а" and status_temp != "о" and status_temp != "з":
+                add_input = input("Желаете изменить статус заметки?(да/нет)").lower()
+                if add_input == "да":
                     status_temp = input(
-                        "Введите статус заметки буквой: А — Активный, О — Отменен, З — Завершен"
-                    ).lower()
+                        "Введите статус заметки буквой: А — Активный, О — Отменен, З — Завершен").lower()
+                    while True:
+                        if status_temp != "а" and status_temp != "о" and status_temp != "з":
+                            status_temp = input(
+                                "Введите статус заметки буквой: А — Активный, О — Отменен, З — Завершен"
+                            ).lower()
+                            break
+                    break
+                elif add_input == 'нет':
                     break
             status_lib = {"а": 'Активный', "о": 'Отменен', "з": 'Завершен'}
-            status = (status_lib[update_status(request)])
+            status = (status_lib[status_temp])
         else:
             print("Неверное имя поля, попробуйте ещё раз")
         print(f'Текущие данные заметки:\n{note}')
